18154f4d476c556d5c7c72ef13a741edc30cd5fb
Vikas Kumar <85861073+vikasatfactors@users.noreply.github.com>
2024-12-05 01:34:12 +0530

    Update cors.go (#19)

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -7,7 +7,7 @@ import (
 
 func CORSConfig() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
+		AllowOrigins:     []string{"https://status-page-app.web.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Authorization", "Content-Type", "Upgrade", "Connection", "Sec-WebSocket-Key", "Sec-WebSocket-Version", "Sec-WebSocket-Protocol"},
 		AllowCredentials: true,
